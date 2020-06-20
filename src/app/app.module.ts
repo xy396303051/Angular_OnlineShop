3d8798e4375e101ff23200189f7fae4cb07efb51
EricZzzZzz <yuhuazhao52@gmail.com>
2020-06-20 19:50:13 +1000

    Basic framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/data.ts
@@ -0,0 +1,59 @@
+products = [
+  {
+    "id": 0,
+    "name": "Ocean Blue Shirt",
+    "description": "<p>Ocean blue cotton shirt with a narrow collar and buttons down the front and long sleeves. Comfortable fit and tiled kalidoscope patterns. </p>",
+    "category": "men",
+    "image_url": "https://burst.shopifycdn.com/photos/young-man-in-bright-fashion_925x.jpg",
+    "unit_cost": 64.95,
+    "inventory": 0
+  },
+  {
+    "id": 1,
+    "name": "Classic Varsity Top",
+    "description": "<p>Womens casual varsity top, This grey and black buttoned top is a sport-inspired piece complete with an embroidered letter. </p>",
+    "category": "women",
+    "image_url": "https://burst.shopifycdn.com/photos/casual-fashion-woman_925x.jpg",
+    "unit_cost": 94.95,
+    "inventory": 40
+  },
+  {
+    "id": 2,
+    "name": "Yellow Wool Jumper",
+    "description": "<p>Knitted jumper in a soft wool blend with low dropped shoulders and wide\u00c2\u00a0sleeves and think cuffs. Perfect for keeping warm during Fall. </p>",
+    "category": "women",
+    "image_url": "https://burst.shopifycdn.com/photos/autumn-photographer-taking-picture_925x.jpg",
+    "unit_cost": 103.95,
+    "inventory": 33
+  },
+  {
+    "id": 3,
+    "name": "Floral White Top",
+    "description": "<p>Stylish sleeveless white top with a floral pattern. </p>",
+    "category": "women",
+    "image_url": "https://burst.shopifycdn.com/photos/city-woman-fashion_925x@2x.jpg",
+    "unit_cost": 136.95,
+    "inventory": 67
+  },
+  {
+    "id": 4,
+    "name": "Striped Silk Blouse",
+    "description": "<p>Ultra-stylish black and red striped silk blouse with buckle collar and matching button pants. </p>",
+    "category": "women",
+    "image_url": "https://burst.shopifycdn.com/photos/striped-blouse-fashion_925x.jpg",
+    "unit_cost": 58.95,
+    "inventory": 79
+  },
+  {
+    "id": 5,
+    "name": "Classic Leather Jacket",
+    "description": "<p>Womans zipped leather jacket. Adjustable belt for a comfortable fit, complete with shoulder pads and front zip pocket. </p>",
+    "category": "women",
+    "image_url": "https://burst.shopifycdn.com/photos/leather-jacket-and-tea_925x.jpg",
+    "unit_cost": 89.95,
+    "inventory": 35
+  },
+  {
+    "id": 6, "name": "Dark Denim Top", "description": "<p>Classic dark denim top with chest pockets, long sleeves with buttoned cuffs, and a ripped hem effect.</p>", "category": "women", "image_url": "https://burst.shopifycdn.com/photos/young-female-models-denim_925x.jpg", "unit_cost": 144.95, "inventory": 65
+  }, { "id": 7, "name": "Navy Sports Jacket", "description": "<p>Long-sleeved navy waterproof jacket\u00c2\u00a0in thin, polyester fabric with a\u00c2\u00a0soft mesh inside. The durable water-repellent finish means you'll be kept comfortable and protected when out in all weathers.</p>", "category": "men", "image_url": "https://burst.shopifycdn.com/photos/mens-fall-fashion-jacket_925x.jpg", "unit_cost": 139.95, "inventory": 10 }, { "id": 8, "name": "Soft Winter Jacket", "description": "<p>Thick black winter jacket, with soft fleece lining. Perfect for those cold weather days.</p>", "category": "women", "image_url": "https://burst.shopifycdn.com/photos/smiling-woman-on-snowy-afternoon_925x.jpg", "unit_cost": 159.95, "inventory": 52 }, { "id": 9, "name": "Black Leather Bag", "description": "<p>Womens black leather bag, with ample space. Can be worn over the shoulder, or remove straps to carry in your hand. </p>", "category": "women", "image_url": "https://burst.shopifycdn.com/photos/black-bag-over-the-shoulder_925x.jpg", "unit_cost": 0.95, "inventory": 18 }, { "id": 10, "name": "Zipped Jacket", "description": "<p>Dark navy and light blue men's zipped waterproof jacket with an outer zipped chestpocket for easy storeage.</p>", "category": "men", "image_url": "https://burst.shopifycdn.com/photos/menswear-blue-zip-up-jacket_925x.jpg", "unit_cost": 90.95, "inventory": 3 }, { "id": 11, "name": "Silk Summer Top", "description": "<p>Silk womens top with short sleeves and number pattern.</p>", "category": "women", "image_url": "https://burst.shopifycdn.com/photos/young-hip-woman-at-carnival_925x.jpg", "unit_cost": 47.95, "inventory": 82 }, { "id": 12, "name": "Long Sleeve Cotton Top", "description": "<p>Black cotton womens top, with long sleeves, no collar and a thick hem. </p>", "category": "women", "image_url": "https://burst.shopifycdn.com/photos/woman-outside-brownstone_925x.jpg", "unit_cost": 15.95, "inventory": 75 }, { "id": 13, "name": "Chequered Red Shirt", "description": "<p>Classic mens plaid flannel shirt with long sleeves, in chequered style, with two chest pockets.</p>", "category": "men", "image_url": "https://burst.shopifycdn.com/photos/red-plaid-shirt_925x.jpg", "unit_cost": 107.95, "inventory": 9 }, { "id": 14, "name": "White Cotton Shirt", "description": "<p>Plain white cotton long sleeved shirt with loose collar. Small buttons and front pocket. </p>", "category": "women", "image_url": "https://burst.shopifycdn.com/photos/smiling-woman-poses_925x.jpg", "unit_cost": 53.95, "inventory": 49 }, { "id": 15, "name": "Olive Green Jacket", "description": "<p>Loose fitting olive green jacket with buttons and large pockets. Multicoloured pattern on the front of the shoulders.</p>", "category": "women", "image_url": "https://burst.shopifycdn.com/photos/urban-fashion_925x.jpg", "unit_cost": 15.95, "inventory": 75 }, { "id": 16, "name": "Blue Silk Tuxedo", "description": "<p>Blue silk tuxedo with marbled aquatic pattern and dark lining. Sleeves are complete with rounded hem and black buttons.</p>", "category": "men", "image_url": "https://burst.shopifycdn.com/photos/man-adjusts-blue-tuxedo-bowtie_925x.jpg", "unit_cost": 152.95, "inventory": 60 }, { "id": 17, "name": "Red Sports Tee", "description": "<p>Women's red sporty t-shirt with colorful details on the sleeves and a small white pocket.</p>", "category": "women", "image_url": "https://burst.shopifycdn.com/photos/womens-red-t-shirt_925x.jpg", "unit_cost": 8.95, "inventory": 97 }, { "id": 18, "name": "Striped Skirt and Top", "description": "<p>Black cotton top with matching striped skirt. </p>", "category": "women", "image_url": "https://burst.shopifycdn.com/photos/woman-in-the-city_925x.jpg", "unit_cost": 97.95, "inventory": 9 }, { "id": 19, "name": "LED High Tops", "description": "<p>Black high top shoes with green LED lights in the sole, tied up with laces and a buckle. </p>", "category": "men", "image_url": "https://burst.shopifycdn.com/photos/putting-on-your-shoes_925x.jpg", "unit_cost": 0.95, "inventory": 75 }]
+
